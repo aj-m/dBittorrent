d7104103c49b63ea929505b87432f7cc8bdd89f6
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-01-28 13:11:56 -0500

    Move qrhandlers includes to the header only

--- include/qrhandlers.h
+++ include/qrhandlers.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <quirc/quirc.h>
 #include <libqrencode/qrencode.h>
 
--- src/qrhandlers.c
+++ src/qrhandlers.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <qrhandlers.h>
 
 quirc_decode_error_t* DecodeQR(){
